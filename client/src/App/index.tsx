aec378ad8f69fed89b804833a53d8082c2fd7ff5
Jake <jakedavisemail@gmail.com>
2023-02-27 12:05:15 -0700

    Small Touches

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -5,7 +5,9 @@ const Field = require('../../../sim/field.ts').default;
 function App () {
   const [field, setField] = React.useState(new Field());
   field.populate()
+  field.generateFood()
   console.log(field.creatures[0])
+  console.log(field.food[1])
 
   return (
     <p>Hello </p>
