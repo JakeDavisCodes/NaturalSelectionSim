7ad6015bc7d9cd5e367eb53d19ae1c4635bb147f
Jake <jakedavisemail@gmail.com>
2023-02-27 10:54:15 -0700

    Compiling Is Good!

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Field = require('../../../sim/public/field.js').default;
+const Field = require('../../../sim/field.ts').default;
 
 function App ({}) {
   const [field, setField] = React.useState(new Field());
   console.log(field)
 
   return (
-    <p>Hello World</p>
+    <p>Hello </p>
   )
 };
 
