ed71984c7a5bb075cf09f9ff3d612310fcae2bc4
Jake <jakedavisemail@gmail.com>
2023-03-01 14:48:03 -0700

    Sighup Func

--- client/src/App/Login.tsx
+++ client/src/App/Login.tsx
@@ -5,10 +5,11 @@ function Display ({ user, setUser }) {
   if (user) return null;
 
   const [username, setUsername] = React.useState('');
+  const [pwd, setPwd] = React.useState('');
   const [hash, setHash] = React.useState('');
 
   const login = () => {
-    if (username.length > 5 && hash > 5) {
+    if (username.length > 5 && pwd > 5) {
       axios({
         method: 'GET',
         url: `/user/:username/:hash`
@@ -29,6 +30,34 @@ function Display ({ user, setUser }) {
           } else {
             alert('User does not exist')
           }
+        })
+        .catch((err) => console.error(err))
+    } else {
+      alert('Please enter this info below: \n\n Password with at least 6 characters \n Username with at least 6 characters')
+    }
+  }
+  const signup = () => {
+    if (username.length > 5 && pwd > 5) {
+      axios({
+        method: 'post',
+        url: `/user`,
+        data: {
+          username,
+          hash,
+        }
+      })
+        .then(() => {
+          setUser(username)
+          const s_id = Math.floor(Math.random() * 10000000);
+          axios({
+            method: 'POST',
+            url: '/session',
+            data: {
+              username,
+              s_id,
+            },
+          })
+          document.cookie += `; s_id=${s_id}`
         })
         .catch((err) => console.error(err))
     } else {
